ef31df9ac8dd5a43df2a5e281ecaa7830512f474
Jaco Erasmus <jaco.erasmus@mwrinfosecurity.com>
2019-01-28 17:41:34 +0200

    Add ratings to referees

--- src/app/referees/referee.model.ts
+++ src/app/referees/referee.model.ts
@@ -1,13 +1,23 @@
+import { Rating } from '../shared/rating.model';
+
 export class Referee {
   public firstName: string;
   public lastName: string;
   public description: string;
   public imagePath: string;
+  public ratings: Rating[];
 
-  constructor(firstName: string, lastName: string, desc: string, imagePath: string) {
+  constructor(
+    firstName: string,
+    lastName: string,
+    desc: string,
+    imagePath: string,
+    ratings: Rating[])
+  {
     this.firstName = firstName;
     this.lastName = lastName;
     this.description = desc;
     this.imagePath = imagePath;
+    this.ratings = ratings;
   }
 }
--- src/app/referees/referee.service.ts
+++ src/app/referees/referee.service.ts
@@ -1,14 +1,31 @@
 import { Referee } from './referee.model';
 import { EventEmitter } from '@angular/core';
+import { Rating } from '../shared/rating.model';
 
 export class RefereeService {
   refereeSelected = new EventEmitter<Referee>();
 
   private referees: Referee[] = [
-    new Referee('Johnathan', 'Kaplan', 'Our foremost Referee',
-      'https://cdn.24.co.za/files/Cms/General/d/2781/3d697212060b41f6a214ff3349397f69.jpg'),
-    new Referee('John', 'Kap', 'Our Referee',
-      'https://cdn.24.co.za/files/Cms/General/d/2781/3d697212060b41f6a214ff3349397f69.jpg')
+    new Referee(
+      'Johnathan',
+      'Kaplan',
+      'Our foremost Referee',
+      'https://cdn.24.co.za/files/Cms/General/d/2781/3d697212060b41f6a214ff3349397f69.jpg',
+      [
+        new Rating(1.0, 0, "A bad day at the office"),
+        new Rating(9.0, 0, "A great day at the office"),
+      ]
+    ),
+    new Referee(
+      'John',
+      'Kap',
+      'Our Referee',
+      'https://cdn.24.co.za/files/Cms/General/d/2781/3d697212060b41f6a214ff3349397f69.jpg',
+      [
+        new Rating(1.0, 0, "A terrible day at the office"),
+        new Rating(9.0, 0, "A magnificent day at the office"),
+      ]
+    )
   ];
 
   getReferees() {
